21e0213955dc1176c531bdfd2a8b253ba67ee101
zhuoliny <zhuoliny1103@outlook.com>
2019-05-28 13:45:07 -0500

    annotation_ems & expr added

---
+++ annotation_ems/gen_dataset.py
@@ -0,0 +1,110 @@
+import os
+import glob
+import math
+import random
+
+random.seed(666)
+
+dataset_path = '/mnt/data/yxchen/gesture-datasets/ems'
+output_path = './annotation_ems'
+
+round = "11.3"
+modality = "rgb" # d, rgb, rgbd
+
+# train: first n
+train_partition = {
+    #'subject01_left_90': 20,
+    #'subject01_left_45': 20,
+    #'subject01_seq_base2': 20,
+    'subject01_human_seq': 10,
+    'subject01_human_seq2': 10,
+    'subject02_human_seq': 20
+}
+
+# test: all except first n
+test_partition = {
+    #'subject01_left_90': 20,
+    #'subject01_left_45': 20,
+    'subject01_seq_base2': 0,
+    'subject01_human_seq': 10,
+    'subject01_human_seq2': 10,
+    'subject02_human_seq': 20
+}
+
+labels = ['wrist_up', 'wrist_down', 'wrist_left', 'wrist_right',
+        'arm_down', 'up_left', 'up_right', 'down_left', 
+        'arm_down_left', 'arm_down_right'] 
+       # 'human_wrist_up', 'human_wrist_down', 
+       # 'human_wrist_left', 'human_wrist_right',
+       # 'human_arm_down', 'human_up_left', 'human_up_right', 'human_down_left', 
+       # 'human_arm_down_left', 'human_arm_down_right']
+
+def get_list(path, dspath, paired=False):
+    if modality == 'd':
+        samples = sorted(glob.glob(os.path.join(path, 'depth/*_all')))
+    elif modality == 'rgb':
+        samples = sorted(glob.glob(os.path.join(path, 'rgb/*_all')))
+    elif modality == 'rgbd':
+        samples = sorted(glob.glob(os.path.join(path, 'rgb/*_all'))) + \
+            sorted(glob.glob(os.path.join(path, 'depth/*_all')))
+
+    #samples = sorted(glob.glob(os.path.join(path, 'rgb/*_all')))
+    random.shuffle(samples)
+    l = {}
+    for s in samples:
+        s = os.path.relpath(s, dspath)
+        label = get_label_id(s, paired=paired)
+        if label != None:
+            l[label] = l.get(label, [])
+            l[label].append(s)
+    return l
+
+def make_dataset(path, paired=False, modality='rgb'):
+    dataset = {}
+    dpath = os.path.join(path, 'data')
+    for d in glob.glob(os.path.join(dpath, '*')):
+        dataset[os.path.relpath(d, dpath)] = get_list(os.path.join(dpath, d), path, paired=paired)
+    
+    return dataset
+
+def get_label_id(path, paired=False):
+    if paired:
+        path = path.split('FOLLOWED_BY')[-1]
+    for i, l in enumerate(labels):
+        if l in path:
+            return i
+    return None
+
+def gen_list(dataset, partition, labels, stage='train'):
+    l = []
+    for k in partition.keys():
+        data = dataset[k]
+        part = partition[k]
+        for i, label in enumerate(labels):
+            if not i in data.keys():
+                continue
+            if stage == 'train':
+                l += [(x, str(i+1)) for x in data[i][:part]]
+            elif stage == 'test':
+                l += [(x, str(i+1)) for x in data[i][part if part!=None else len(data[i]):]]
+            else:
+                raise NotImplementedError()
+    return l
+
+def write_list(l, path):
+    with open(path, 'w') as f:
+        f.write('\n'.join([' '.join(x) for x in l]))
+
+def write_labels(labels, path):
+    class_ind = [' '.join((str(i+1), x)) for i, x in enumerate(labels)]
+    with open(path, 'w') as f:
+        f.write('\n'.join(class_ind))
+
+labels.sort(key=lambda item: (-len(item), item))
+
+dataset = make_dataset(dataset_path, paired=True, modality=modality)
+train_list = gen_list(dataset, train_partition, labels, 'train')
+test_list = gen_list(dataset, test_partition, labels, 'test')
+write_list(train_list, os.path.join(output_path, 'trainlist' + round + '.txt'))
+write_list(test_list, os.path.join(output_path, 'testlist' + round + '.txt'))
+write_labels(labels, os.path.join(output_path, 'classInd' + round + '.txt'))
---
+++ expr/ems05.5.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# train classifier
+CUDA_VISIBLE_DEVICES=1 python main.py \
+	--root_path /home/yxchen/Real-time-GesRec \
+	--video_path /mnt/data/yxchen/gesture-datasets/ems \
+	--annotation_path annotation_ems/ems05.5.json\
+	--result_path results/ems05.5 \
+	--pretrain_path /mnt/data/yxchen/model-zoo/jester_resnext_101_RGB_32.pth \
+	--dataset ems \
+	--sample_duration 32 \
+    --learning_rate 0.01 \
+    --model resnext \
+	--model_depth 101 \
+	--resnet_shortcut B \
+	--batch_size 8 \
+	--n_classes 27 \
+	--n_finetune_classes 4 \
+	--n_threads 16 \
+	--checkpoint 1 \
+	--modality RGB \
+	--train_crop random \
+	--n_val_samples 1 \
+	--test_subset test \
+    --n_epochs 100 \
