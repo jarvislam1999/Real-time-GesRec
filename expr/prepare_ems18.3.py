308da9403c6e3b74f95286afd8c01453ebd52066
Yuxin Chen <chenyuxin.mail@gmail.com>
2019-06-24 13:46:07 -0500

    ems18 - ems18.5

---
+++ expr/prepare_ems18.3.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append('.')
+
+from annotation_ems.gen_dataset import generate_dataset
+from annotation_ems.ems_prepare import split, prepare_json
+
+### begin of config
+
+dataset_path = '/fastdata/yxchen/gesture-datasets/ems'
+output_path = './annotation_ems'
+
+expr_name = "18.3"
+modality = "rgb"  # d, rgb, rgbd
+
+# train: first n
+train_partition = {
+    'subject03_machine_recovery_3gps_2pairs_60fps_2': 50,
+}
+
+# test: all except first n
+test_partition = {
+    'subject03_machine_recovery_3gps_2pairs_60fps_2': 50,
+}
+
+labels = ['wrist_left', 'wrist_right', 'pronation', 'supination']
+
+### end of config
+
+generate_dataset(expr_name=expr_name, modality=modality, dataset_path=dataset_path, output_path=output_path, train_partition=train_partition, test_partition=test_partition, labels=labels)
+
+prepare_json(csv_dir_path=output_path, expr_name=expr_name)
