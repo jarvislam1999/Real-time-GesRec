73bb4880fba5eacfb76c9d9fecefe301712a0fa4
Jarvis Lam <jarvislam1999@gmail.com>
2019-07-12 10:36:26 -0500

    Newest experiments, more data needed

---
+++ expr/prepare_ems22.12.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.append('.')
+
+from annotation_ems.gen_dataset import generate_dataset
+from annotation_ems.ems_prepare import split, prepare_json
+
+### begin of config
+
+dataset_path = '/mnt/data/yxchen/gesture-datasets/ems/'
+output_path = './annotation_ems'
+
+expr_name = "22.12"
+modality = "rgb"  # d, rgb, rgbd
+
+# train: first n
+train_partition = {
+}
+
+# test: all except first n
+test_partition = {
+    #'subject01_setting3_bg02_01': 0,
+    #'subject01_setting3_bg03_01': 0,
+    'subject01_setting3_bg04_01': 0,
+}
+
+labels = ['wrist_left', 'wrist_right', 'pronation', 'supination']
+
+### end of config
+
+generate_dataset(expr_name=expr_name, modality=modality, dataset_path=dataset_path, output_path=output_path, train_partition=train_partition, test_partition=test_partition, labels=labels)
+
+prepare_json(csv_dir_path=output_path, expr_name=expr_name)
